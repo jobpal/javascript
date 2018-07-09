7982979328e95c173e81f3b292a8d618911e0cf0
Sam Rendall <sam@jobpal.ai>
2018-07-09 10:40:12 +0200

    allow for unary operators in loop afterthoughts

--- packages/eslint-config-jobpal/index.js
+++ packages/eslint-config-jobpal/index.js
@@ -5,6 +5,7 @@ module.exports = {
   rules: {
     semi: ['error', 'never', {
       'beforeStatementContinuationChars': 'any',
-    }]
+    }],
+    'no-plusplus': ['error', { 'allowForLoopAfterthoughts': true }]
   }
 }
